d6988f041e99e70c5c536d302809590c89de5933
nabildev <nabilbayad969@gmail.com>
2024-01-16 22:03:18 +0100

    frontend: Experiences section

---
+++ frontend/src/data/experiences.js
@@ -0,0 +1,23 @@
+export const experiences = [
+    {
+        _id: "nhyryjtrtjur-èjè(j(85767867858",
+        company: "company 1",
+        title: "web developer",
+        period: "2023",
+        description: "",
+    },
+    {
+        _id: "nhyryhio867858",
+        company: "company 2",
+        title: "web developer",
+        period: "2022",
+        description: "",
+    },
+    {
+        _id: "nhyryjtrtj3475767867858",
+        company: "company 3",
+        title: "desktop developer",
+        period: "2022",
+        description: "",
+    },
+]
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../../components/header';
 import Intro from './intro';
 import About from './About';
+import Experiences from '../Experiences';
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
       <div className='bg-primary px-40 sm:px-5'>
       <Intro />
       <About />
+      <Experiences />
       </div>
     </div>
   )
